5c7f573b0b33e25871f12b8b7e9385c1fcee39e8
Daniel Lugo <gh@danlugo.dev>
2025-08-29 09:13:26 -0500

    Remove unneded webpack

--- dist/built/src/Math.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const sigmoid: (n: number) => number;
-export declare const sigmoidPrime: (z: number) => number;
-export declare const softplus: (n: number) => number;
-export declare const rnd: (low?: number | undefined, high?: number | undefined) => number;
-export declare const gaussRnd: () => number;
